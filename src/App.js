9aaa358af1b0ba2582fc24e576447d1ae171a9c2
koladiya-nikunj <koladiyanikunj124@gmail.com>
2024-03-02 17:27:38 +0530

    One tao login success message

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 import { useGoogleOneTapLogin } from "react-google-one-tap-login"
+import { useScreenshot } from "use-react-screenshot";
 
 function App() {
+  const ref = useRef(null);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [showPopup, setShowPopup] = useState(true);
+  const [error] = useScreenshot({
+    type: "image/jpeg",
+    quality: 1.0,
+  });
+
+  if (error) {
+    console.error("Error occurred during screenshot capture:", error);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (showPopup && !event.target.closest('.google-one-tap-login-popup')) {
+        setShowPopup(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showPopup]);
 
   useGoogleOneTapLogin({
-    onSuccess: (response) => console.log(response),
+    onSuccess: (response) => {
+      setName(response.given_name);
+      setEmail(response.email);
+      setShowPopup(true); 
+      console.log("User email:", response.email);
+    },
     onError: (error) => console.log(error),
     googleAccountConfigs: {
-      client_id: "1027485564712-nm6m9eifqopa3eqq2pnmj83vljb0e74c.apps.googleusercontent.com"
-    }
-  })
+      client_id: "1027485564712-nm6m9eifqopa3eqq2pnmj83vljb0e74c.apps.googleusercontent.com",
+      showPrompt: showPopup
+    },
+  });
+
   return (
-    <div className="app">
-
-
+    <div style={{ width: '100%', textAlign: "center", }} ref={ref} className="app">
+      {email && (
+        <h1 style={{marginTop:'25%'}}>
+          Dear user {name} you are successfully verify with
+          <br />
+          {email}
+        </h1>
+      )}
+      <div>
+        
+      </div>
     </div>
   );
 }
